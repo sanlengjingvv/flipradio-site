a85a3615926ce29c8a95d4ac8fba5f49e8839fe4
nico <lankeren123543@gmail.com>
2025-05-03 16:52:39 +0900

    Order items by release_date

--- app/models/flip_item.rb
+++ app/models/flip_item.rb
@@ -2,7 +2,7 @@ class FlipItem < ApplicationRecord
   validates :title, presence: true
   validates :link, presence: true, uniqueness: true
 
-  scope :recent, -> { order(created_at: :desc) }
+  scope :recent, -> { order(release_date: :desc, created_at: :desc) }
 
   def self.search(query)
     where("title like ? OR content like ?", "%#{query}%", "%#{query}%")
